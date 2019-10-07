cd6d3008a3979aef63f0f37c943565e79144e7c6
Daniel de Vries <daniel.devries@darcorp.com>
2019-10-07 11:34:57 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 exec(open("af_opt/version.py").read())
 
@@ -10,8 +10,8 @@ setup(
     description="Airfoil Optimization using OpenMDAO",
     author="Daniel de Vries",
     author_email="danieldevries6@gmail.com",
-    packages=["af_opt"],
-    install_requires=["numpy>=1.17", "openmdao>=2.8", "differential_evolution"],
+    packages=find_packages(),
+    install_requires=["numpy>=1.17", "openmdao>=2.8", "cst", "differential_evolution", "matplotlib"],
     url="https://github.com/daniel-de-vries/airfoil-optimizer",
     download_url="https://github.com/daniel-de-vries/airfoil-optimizer/archive/v{0}.tar.gz".format(
         __version__
